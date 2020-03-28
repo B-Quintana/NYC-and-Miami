4d64db02f3295a0953aaa8c6d074a658fa908609
Bienvenido Quintna <BienvenidoQuintana34@gmail.com>
2020-03-28 12:37:35 -0400

    Initial commit

---
+++ github/public/scr/img/js/home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navbar } from "./navbar";
+import { Cards } from "./cards";
+import { Jumbotron } from "./jumbotron";
+let va = [
+  {
+    title: 'NYC Train',
+    link:  "https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg",
+    description: 'Inwoods',
+    imgUrl: "https://s.abcnews.com/images/US/subway-1-gty-file-ml-190524_hpMain_4x3_992.jpg",
+  },
+  {
+    title: 'Cool Train',
+    link:   "https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg",
+    description: 'This train was created in 1919',
+    imgUrl: "https://s3-prod.crainsnewyork.com/s3fs-public/BLOGS04_160519867_AR_-1_STFYVAUTRXQG.jpg",
+  },
+  {
+    title: 'White Bullter Train',
+    link:  "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg",
+    description: 'Fast train',
+    imgUrl:  "https://upload.wikimedia.org/wikipedia/commons/7/70/Miami_Metrorail_Hitachi_train_20190117.jpg"
+  },
+]
+export function Home() {
+	return (
+		<>
+			<Navbar />
+			<Jumbotron />
+
+			<div className="cards-group d-flex justify-content-center">
+				{va.map((item, index) => (
+					<Cards key={index} myimage={item} />
+				))}
+			</div>
+		</>
+	);
+}
